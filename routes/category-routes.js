53b6c8d05a606aa557d5f2b6448ffc3843568655
hanna alemu <hanna9alemu@gmail.com>
2019-08-20 17:32:32 -0700

    modularized routes and db in routes.js

---
+++ routes/category-routes.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const express = require('express');
+
+const router = express.Router();
+
+let db = [];
+
+
+// Route to Get All Categories
+router.get('/categories', (req, res, next) => {
+
+  let count = db.length;
+  let results = db;
+  res.json({ count, results });
+  console.log(req.requestTime);
+
+});
+
+// Route to Create a Category
+
+router.post('/categories', (req, res, next) => {
+  let record = req.body;
+  record.id = Math.random();
+  res.json(record);
+  if(req.valid === true) {
+    db.push(record);
+  }else {
+    error500();
+  }
+  next();
+});
+
+module.exports = router;
